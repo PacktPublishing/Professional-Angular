cf5d36e89480d39e4807174d269480111733c4f7
Ekpo David <ekpodave@gmail.com>
2017-11-03 18:37:47 +0100

    Fixed typo in component name

--- mobile_auction/src/components/app/app.module.ts
+++ mobile_auction/src/components/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MobileActionAppComponent }  from './app.component';
+import { MobileAuctionAppComponent }  from './app.component';
 import {AuctionMarketComponent} from '../auction-market/auction-market.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [MobileActionAppComponent, AuctionMarketComponent],
-  bootstrap: [MobileActionAppComponent]
+  declarations: [MobileAuctionAppComponent, AuctionMarketComponent],
+  bootstrap: [MobileAuctionAppComponent]
 })
 export class AppModule { }
