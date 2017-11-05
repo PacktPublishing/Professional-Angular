474a44a1fbcae69be3bd3d71699dcf578e0c5704
Ekpo David <ekpodave@gmail.com>
2017-11-05 20:46:37 +0100

    added DomSanitizer within the component

--- mobile_auction/src/components/auction-market/product-preview/product-preview.component.ts
+++ mobile_auction/src/components/auction-market/product-preview/product-preview.component.ts
@@ -13,8 +13,14 @@ export class ProductPreviewComponent implements OnChanges {
   auctionPreview: ProductAuction[];
   safeProductLinks: Array<SafeResourceUrl>;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
   ngOnChanges() {
     this.auctionPreview = this.auction.productAuctions.slice(this.currentProductAuction + 1, this.currentProductAuction + 4);
-
+    this.safeProductLinks = this.auctionPreview.map(
+      productAuction => {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(productAuction.product.specLink)
+      }
+    )
   }
 }
