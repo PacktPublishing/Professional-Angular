c40ae31bf031d2f87d2fc1aabb7262848e8ed4a2
Ekpo David <ekpodave@gmail.com>
2017-11-26 19:47:11 +0100

    Added shared module

--- mobile_auction/src/components/auction-market/auction-market.module.ts
+++ mobile_auction/src/components/auction-market/auction-market.module.ts
@@ -1,15 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './../shared/shared.module';
+
 import { AuctionMarketComponent } from '../auction-market/auction-market.component';
 import { ProductPreviewComponent } from './product-preview/product-preview.component';
 import { UsersBidsComponent } from './users-bids/users-bids.component';
-import { SecondsToTimePipe } from './seconds-to-time.pipe';
-import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AuctionMarketComponent, ProductPreviewComponent, UsersBidsComponent, SecondsToTimePipe, ReversePipe],
+  imports: [BrowserModule, SharedModule],
+  declarations: [
+    AuctionMarketComponent, 
+    ProductPreviewComponent, 
+    UsersBidsComponent
+  ],
   exports: [AuctionMarketComponent]
 })
 export class AuctionMarketModule { }
