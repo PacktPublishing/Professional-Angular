0d8cfa98570c382acc7f2e9b0ffb84c7a70d0fe2
Ekpo David <ekpodave@gmail.com>
2017-11-27 12:07:07 +0100

    removed product modal

--- mobile_auction/src/components/auction-market/auction-market.module.ts
+++ mobile_auction/src/components/auction-market/auction-market.module.ts
@@ -6,17 +6,14 @@ import { SharedModule } from './../shared/shared.module';
 import { AuctionMarketComponent } from '../auction-market/auction-market.component';
 import { ProductPreviewComponent } from './product-preview/product-preview.component';
 import { UsersBidsComponent } from './users-bids/users-bids.component';
-import { ProductModalComponent } from './product-preview/product-modal.component';
 
 @NgModule({
   imports: [BrowserModule, SharedModule],
   declarations: [
     AuctionMarketComponent, 
     ProductPreviewComponent, 
-    UsersBidsComponent,
-    ProductModalComponent
+    UsersBidsComponent
   ],
-  exports: [AuctionMarketComponent],
-  entryComponents:[ProductModalComponent]
+  exports: [AuctionMarketComponent]
 })
 export class AuctionMarketModule { }
