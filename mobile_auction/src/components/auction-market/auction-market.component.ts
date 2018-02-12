043feae80ceeb05789512edb2fce5bcad4ba7407
Ekpo David <ekpodave@gmail.com>
2018-02-12 14:06:58 +0100

    updated to v5

--- mobile_auction/src/components/auction-market/auction-market.component.ts
+++ mobile_auction/src/components/auction-market/auction-market.component.ts
@@ -249,7 +249,8 @@ export class AuctionMarketComponent implements OnInit {
     this.user = this.getUser()
     if (this.user && this.productAuctionRunningDuration < this.currentProductAuction.duration) {
       if (this.bidAmount > this.currentProductAuction.product.sellingPrice) {
-        this.bid = new Bid(+this.bidAmount, this.user)
+        this.errorMessages.bidError = '';
+        this.bid = new Bid(+this.bidAmount, this.currentProductAuction.product.title, this.user)
         this.currentProductAuction.product.sellingPrice = this.bid.amount;
         localStorage.setItem('productSellingPrice', this.currentProductAuction.product.sellingPrice.toString())
       }
