bd63a22e892327bc8e31a8d1cb96c6adb5513f6f
Ekpo David <ekpodave@gmail.com>
2018-03-18 19:06:37 +0100

    nav fix

--- mobile_auction/src/components/admin/navigation/side-nav.component.ts
+++ mobile_auction/src/components/admin/navigation/side-nav.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { Observable, Subscription } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { ProductAuction, Product } from './../../../services/model';
 import { AuctionService } from './../../../services/auction.service';
@@ -10,14 +10,14 @@ import { AuctionBuilderService } from './../admin-services/auction-builder.servi
     selector: 'side-nav',
     templateUrl: '/src/components/admin/navigation/side-nav.component.html'
 })
-export class SideNavComponent implements OnInit, OnDestroy {
+export class SideNavComponent implements OnInit {
     preset: Object = {
         route: '/admin/auction/new',
         title: 'Auctions',
         newButton: 'New Auction'
     };
     showNav: boolean = true;
-    productAuctions: Observable<ProductAuction[]>;
+    productAuctions: ProductAuction[];
     subscription: Subscription;
 
     constructor(
@@ -29,46 +29,31 @@ export class SideNavComponent implements OnInit, OnDestroy {
     ngOnInit(){
         this.generateLinks(this.router.url);
         this.hideNav(this.router.url);
-        this.subscription = this.router.events.subscribe(
-            (navigationEnd: NavigationEnd) => {
-                let route = navigationEnd.url;
-
-                if (route.includes('product-auction')) {
-                    this.preset = {
-                        route: '/admin/product-auction/new',
-                        title: 'Product Auctions',
-                        newButton: 'New Product Auction'
-                    };
-                    this.hideNav(route);
-                }
-                else {
-                    this.preset = {
-                        route: '/admin/auction/new',
-                        title: 'Auctions',
-                        newButton: 'New Auction'
-                    };
-                    this.generateLinks(route);
-                    this.hideNav(route);
-                }
-            }
-        )
     }
 
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
+    setPreset(preset: any) {
+        this.preset = preset;
+        this.generateLinks(preset.route);
+        this.hideNav(preset.route);
     }
 
     generateLinks(route: string) {
-        if (route.includes('auction/')){
-            this.productAuctions = this.auctionService.getProductAuctions();
+        if (route.includes('admin/auction')){
+            this.auctionService.getProductAuctions()
+            .subscribe(
+                (productAuctions: ProductAuction[]) => {
+                    this.productAuctions = productAuctions;
+                },
+                (err: any) => console.error(err)
+            );
         }
     }
     hideNav(route: string){
-        if (route.includes('product-auction/')) {
-            this.showNav = false;
+        if (route.includes('admin/auction')) {
+            this.showNav = true;
         }
         else{
-            this.showNav = true;
+            this.showNav = false;
         }
     }
     addProduct(product: Product) {
