d81ca5e8c8d41977275adf2b30079c32abcad9ae
Ekpo David <ekpodave@gmail.com>
2018-03-18 19:06:37 +0100

    nav fix

--- mobile_auction/src/components/admin/admin.component.ts
+++ mobile_auction/src/components/admin/admin.component.ts
@@ -3,11 +3,11 @@ import {Component} from '@angular/core';
 @Component({
     template: `
         <div class="sub-nav">
-            <sub-nav></sub-nav>
+            <sub-nav (changeSideNav)="sideNav.setPreset($event)"></sub-nav>
         </div>
         <div class="row">
             <div class="col-sm-2 side-pane-right">
-                <side-nav></side-nav>
+                <side-nav #sideNav></side-nav>
             </div>
             <div class="col-sm-10">
                 <div class="m-r-md m-l-md auction">
