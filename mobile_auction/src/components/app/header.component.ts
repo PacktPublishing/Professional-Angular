979985cc106b4e600dbcf5b18b9fd3c03d3fa35e
Ekpo David <ekpodave@gmail.com>
2018-03-18 22:57:23 +0100

    index link

--- mobile_auction/src/components/app/header.component.ts
+++ mobile_auction/src/components/app/header.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
     <nav class="navbar navbar-default m-b-n">
       <div class="container-fluid">
         <div class="navbar-header">
-          <h1>Mobile Auction</h1>
+          <h1><a class="m-r-md" [routerLink]="['/']">Mobile Auction</a></h1>
         </div>
         <div class="navbar-form navbar-right m-t-md">
           <a class="m-r-md" [routerLink]="['/admin']">
